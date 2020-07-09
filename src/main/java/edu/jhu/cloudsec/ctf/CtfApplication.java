b17ff586b04199b45784dd88bf5f7b61617104db
Zach Romano <zacharyr24@hotmail.com>
2020-07-08 19:46:30 -0700

    Initial Commit

---
+++ src/main/java/edu/jhu/cloudsec/ctf/CtfApplication.java
@@ -0,0 +1,13 @@
+package edu.jhu.cloudsec.ctf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/jhu/cloudsec/ctf/CtfApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jhu.cloudsec.ctf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
