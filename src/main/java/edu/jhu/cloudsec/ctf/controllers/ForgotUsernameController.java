e8be7be1950578c8778144910a65cf7ce1dc86af
Zach Romano <zacharyr24@hotmail.com>
2020-07-22 20:37:18 -0700

    fix database credentials

--- src/main/java/edu/jhu/cloudsec/ctf/controllers/ForgotUsernameController.java
+++ src/main/java/edu/jhu/cloudsec/ctf/controllers/ForgotUsernameController.java
@@ -3,6 +3,7 @@ package edu.jhu.cloudsec.ctf.controllers;
 import edu.jhu.cloudsec.ctf.entities.InsecureVoter;
 import edu.jhu.cloudsec.ctf.services.H2InjectionChecker;
 import edu.jhu.cloudsec.ctf.services.UnsafeH2DatabaseConnection;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class ForgotUsernameController {
+    private final UnsafeH2DatabaseConnection unsafeH2DatabaseConnection;
 
     @RequestMapping("/forgot")
     public String forgot() {
@@ -25,7 +28,7 @@ public class ForgotUsernameController {
         if (H2InjectionChecker.inputCouldModifyTable(userSearch)) {
             model.addAttribute("error", "Looks like you might be trying to modify the database! You could be arrested for hacking!");
         } else {
-            List<InsecureVoter> insecureVoterList = UnsafeH2DatabaseConnection.findAllUsersByFirstName(userSearch);
+            List<InsecureVoter> insecureVoterList = unsafeH2DatabaseConnection.findAllUsersByFirstName(userSearch);
             model.addAttribute("results", insecureVoterList);
         }
         return "search";
--- src/main/java/edu/jhu/cloudsec/ctf/services/UnsafeH2DatabaseConnection.java
+++ src/main/java/edu/jhu/cloudsec/ctf/services/UnsafeH2DatabaseConnection.java
@@ -1,6 +1,8 @@
 package edu.jhu.cloudsec.ctf.services;
 
 import edu.jhu.cloudsec.ctf.entities.InsecureVoter;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.sql.*;
@@ -8,18 +10,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class UnsafeH2DatabaseConnection {
-    private UnsafeH2DatabaseConnection() {
-    }
+    private final Environment environment;
 
-    public static List<InsecureVoter> findAllUsersByFirstName(String firstName) {
+    public List<InsecureVoter> findAllUsersByFirstName(String firstName) {
+        final String databaseUrl = environment.getProperty("spring.datasource.url");
+        final String databaseUsername = environment.getProperty("spring.datasource.username");
+        final String databasePassword = environment.getProperty("spring.datasource.password");
+
         Statement stmt = null;
         String query = "SELECT ID, USERNAME, FIRST_NAME, LAST_NAME " +
                 "FROM VOTERS WHERE FIRST_NAME = '" + firstName + "'";
 
         Connection conn = null;
         try {
-            String url = "jdbc:h2:mem:testdb;USER=sa;PASSWORD=password";
+            String url = databaseUrl + ";USER=" + databaseUsername + ";PASSWORD=" + databasePassword;
             conn = DriverManager.getConnection(url);
 
             stmt = conn.createStatement();
